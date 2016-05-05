b8b5099fce2cbb5e93f9512ad44e0f37c6b584f5
tsurugi197 <pixeletedprince@gmail.com>
2016-05-06 00:06:10 +0300

    Js remarks

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,19 @@
-var obj = document.getElementById("search-form");
-var searchBtn = document.getElementById("caller-btn");
-searchBtn.addEventListener("click", function(event) {
+var call_btn = document.querySelector(".caller-btn");
+var search_form = document.querySelector(".search-form");
+var search_submit = document.querySelector(".search-btn");
+
+var datein = search_form.querySelector("[name=datein]");
+var dateout = search_form.querySelector("[name=dateout]");
+var adult = search_form.querySelector("[name=adult]");
+var kids = search_form.querySelector("[name=kids]");
+
+call_btn.addEventListener("click", function(evenet) {
 	event.preventDefault();
-	obj.classList.toggle("show-hide");
-}, false);
+	search_form.classList.toggle("search-animation");
+});
+search_submit.addEventListener("click", function(event) {
+	if (!datein.value || !dateout.value || !adult.value || !kids.value) {
+		event.preventDefault();
+		alert("Please, fill all fields.... Hey, you, please take me frome here, they force me to work all the ti....i can't speak any mo....ah u little brat! NOO PLEASE(*very painfull screams*)");
+	}
+});
